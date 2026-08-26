5529e50bfde07af410aed196415995c442890bc6
connorth3-lgtm <connorth3@gmail.com>
2026-08-26 12:00:59 +1200

    Keep learning UX on current offline cache lineage

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_VERSION='2026.08.24.1';
-const CACHE_REVISION='learning-flow-20260826';
+const CACHE_REVISION='evidence-maturity-triangulation-20260826';
 const STATIC_CACHE=`mouldmaster-static-${CACHE_VERSION}-${CACHE_REVISION}`;
 const CORE=[
   './index.html',
