a34aa629020ef1d712915c12995276e08df48c0b
connorth3-lgtm <connorth3@gmail.com>
2026-09-24 17:49:21 +1200

    fix: advance governed web runtime release

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='2026.09.24.6';
+const CACHE_VERSION='2026.09.24.7';
 const CACHE_REVISION='lifecycle-r5-20260924';
 const STATIC_CACHE=`mouldmaster-static-${CACHE_VERSION}-${CACHE_REVISION}`;
 
